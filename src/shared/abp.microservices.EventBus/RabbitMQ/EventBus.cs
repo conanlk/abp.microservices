0647031387e37d6ccd28b1de965a42232f718aaa
au.nguyen <au.nguyen@evehr.vn>
2024-04-24 10:03:59 +0700

    update code

--- src/shared/abp.microservices.EventBus/RabbitMQ/EventBus.cs
+++ src/shared/abp.microservices.EventBus/RabbitMQ/EventBus.cs
@@ -2,5 +2,6 @@ namespace abp.microservices.EventBus.RabbitMQ;
 
 public class EventBus
 {
-    
+    public const string Channel = "abp.microservervice.{0}";
+    public const string DeadLetterChannel = "abp.microservervice.deadletter.{0}";
 }
